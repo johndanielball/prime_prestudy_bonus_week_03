5399e14500f36b28c0fef58f944272b70d715bc4
John Ball <johndanielball86@gmail.com>
2015-12-20 16:13:08 -0600

    Prime prestudy bonus week 03
    JavaScript

---
+++ prime_prestudy_bonus_week_03/index.js
@@ -0,0 +1,11 @@
+﻿function functionA(a) {
+    return a * a;
+}
+function functionB(a) {
+    return a + a;
+}
+function functionC(a) {
+    return a / a;
+}
+
+console.log(functionA(functionB(functionC(10))));
